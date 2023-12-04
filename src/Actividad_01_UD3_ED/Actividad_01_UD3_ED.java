29b942e2797b0ebf25951bd52e154d519bdfd8c3
inuncab <inuncab924@g.educaand.es>
2023-12-04 12:19:54 +0100

    Traduccion de variables

---
+++ src/Actividad_01_UD3_ED/Actividad_01_UD3_ED.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Actividad_01_UD3_ED;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author ana
+ */
+public class Actividad_01_UD3_ED 
+{
+    private static byte[] intermedio = new byte[1000];
+    private static String nombrearchivo = "fichero.dat";
+    private static FileInputStream flujoentrada = null;
+    private static BufferedInputStream buferentrada = null;
+
+    public static void inicializateFiles() throws FileNotFoundException
+    {
+       flujoentrada = new FileInputStream(nombrearchivo);
+        buferentrada = new BufferedInputStream(flujoentrada);
+    }
+    
+    public static int showFileText() throws IOException
+    {
+        int total = 0;
+        int nRead = 0;
+        while((nRead = flujoentrada.read(intermedio)) != -1) 
+        {
+            System.out.println(new String(intermedio));
+            total += nRead;
+        }
+        
+        return total;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {        
+        try 
+        {
+            inicializateFiles();
+            
+            int total = showFileText();           
+
+            System.out.println("\nLeídos " + total + " bytes");
+        }
+        catch(IOException ex) 
+        {
+            System.out.println("Error -> " + ex.toString());                
+        }
+        finally 
+        {
+            try 
+            {
+                if( buferentrada != null && flujoentrada != null )
+                {
+                    flujoentrada.close();
+                    buferentrada.close();
+                }                
+            } 
+            catch (IOException ex) 
+            {
+                System.out.println("Error al cerrar el fichero -> " + ex.toString());
+            }
+        }
+    }
+    
+}
